20d55c828f58c48e62ff853d26092cac8c2425d9
blacpeaz <chidiigweonu@gmail.com>
2023-05-25 20:37:16 +0300

    Added link to source code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchForm />
+      <p>
+        <a href="https://github.com/blacpeaz/weather-react" target="_blank">
+          Open Source Code{" "}
+        </a>{" "}
+        by Lalao
+      </p>
     </div>
   );
 }
